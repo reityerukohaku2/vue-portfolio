fd49a234ddd2732e7e53a33c6cf4a65ec2a13fb4
reityerukohaku <long.coat.chihaha.kohaku@gmail.com>
2020-09-26 15:52:10 +0900

    about完成

--- src/App.vue
+++ src/App.vue
@@ -28,16 +28,24 @@
       <div class="top">
         <img src="./assets/hikari-logo.png" width="500" height="500" alt />
         <div class="page-title">
-          <h1>ハッピーバースデーヒカリ！！</h1>
+          <h1>
+            ハッピーバースデーヒカリ！！
+          </h1>
           <h2>ヒカリのげぇむじっきょぉ</h2>
         </div>
       </div>
 
       <div class="about" id="about" align="left">
         <h3>ヒカリとは</h3>
-      </div>
-      <div class="about-text">
-        <p>西ノ国からYoutube界に舞い降りた問題児</p>
+        <div class="about-text">
+          <p>
+            令和二年―六月―二十七日―――西ノ国からYoutube界に舞い降りた問題児．実家では母親と同じ部屋で暮らし，動画作成，歌ってみたをしたくてもできなかった．
+          </p>
+          <p>
+            一人暮らしを始め，抑圧から解き放たれた彼は自由を手に入れた．彼を止める者は誰もいない…<br />
+            たとえ友人に「気持ち悪い」「直視できない」と言われても…彼は止まらない…！
+          </p>
+        </div>
       </div>
     </div>
   </div>
